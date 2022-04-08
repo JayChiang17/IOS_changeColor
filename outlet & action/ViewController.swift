9c762d9e4ee7c17b1da39bc8d3afc620e58a0e6b
JayChiang17 <chiang.mengchieh@gmail.com>
2022-04-07 17:13:15 -0700

    Initial Commit

---
+++ outlet & action/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  outlet & action
+//
+//  Created by Meng Chieh Chiang on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
